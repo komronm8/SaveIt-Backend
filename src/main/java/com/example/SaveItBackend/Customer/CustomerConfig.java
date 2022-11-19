d3ef60598e2b39a68cf0958b804ceed3947089ac
Komron <komron.ba@gmail.com>
2022-11-19 02:04:50 +0100

    Created class CustomerConfig to make some tests for repository and database

---
+++ src/main/java/com/example/SaveItBackend/Customer/CustomerConfig.java
@@ -0,0 +1,38 @@
+package com.example.SaveItBackend.Customer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class CustomerConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository repository){
+        return args -> {
+            Customer Mike = new Customer(
+                    "Mike",
+                    49695267583L,
+                    "Mike@gmail.com",
+                    "ExamplePassword123",
+                    LocalDate.of(2002, 1, 12)
+            );
+
+            Customer Alex = new Customer(
+                    "Alex",
+                    496345267583L,
+                    "Alex@gmail.com",
+                    "ExamplePassword456",
+                    LocalDate.of(2000, 12, 12)
+            );
+
+            repository.saveAll(
+                    List.of(Mike, Alex)
+            );
+
+        };
+    }
+}
