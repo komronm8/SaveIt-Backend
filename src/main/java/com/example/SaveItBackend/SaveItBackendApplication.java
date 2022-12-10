7f7ea0b292fa1017a1e30868a1030520e0150a92
Komron <komron.ba@gmail.com>
2022-12-10 22:58:58 +0100

    Added BCrypt password encoder Bean

--- src/main/java/com/example/SaveItBackend/SaveItBackendApplication.java
+++ src/main/java/com/example/SaveItBackend/SaveItBackendApplication.java
@@ -2,12 +2,20 @@ package com.example.SaveItBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SaveItBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaveItBackendApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
