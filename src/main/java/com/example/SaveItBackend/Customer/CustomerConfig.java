d902c8786e452e99d40aad0e0b00aeda14755542
Komron <komron.ba@gmail.com>
2022-12-10 22:52:52 +0100

    Changed version of password encoding of customers

--- src/main/java/com/example/SaveItBackend/Customer/CustomerConfig.java
+++ src/main/java/com/example/SaveItBackend/Customer/CustomerConfig.java
@@ -3,6 +3,7 @@ package com.example.SaveItBackend.Customer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,7 +18,7 @@ public class CustomerConfig {
                     "Mike",
                     49695267583L,
                     "Mike@gmail.com",
-                    "aab2a68fcf5278315da3a66d8dd803e32e1190d8e7ae8d421d874ae018935cf9",
+                    new BCryptPasswordEncoder().encode("moefoe"),
                     LocalDate.of(2002, 1, 12)
             );
 
@@ -25,7 +26,7 @@ public class CustomerConfig {
                     "Alex",
                     496345267583L,
                     "Alex@gmail.com",
-                    "42c81568824258f7f6216337be7d381de744461dd58dad9c67ad20e58e57b729",
+                    new BCryptPasswordEncoder().encode("klia0494"),
                     LocalDate.of(2000, 12, 12)
             );
 
@@ -33,7 +34,7 @@ public class CustomerConfig {
                     "Invoker",
                     491345728453L,
                     "sigmaMale@gmail.com",
-                    "8fc4d17c104401ed4b8eaab91cb7d2f8ad342bb2bb5216f7ba86ef22d265b107",
+                    new BCryptPasswordEncoder().encode("examplepassword"),
                     LocalDate.of(1090, 3, 23)
             );
 
