d36175fe235e6e1b4c4c7ba9a5d9c7ab4e7cc0d6
Komron <komron.ba@gmail.com>
2023-03-05 16:26:24 +0100

    Added another Query for retrieving details for Day History Orders

--- src/main/java/com/example/SaveItBackend/Order/OrderRepository.java
+++ src/main/java/com/example/SaveItBackend/Order/OrderRepository.java
@@ -19,4 +19,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT o FROM Order o WHERE o.store.id = ?1 AND o.orderDate = ?2")
     List<Order> findStoreOrders(Long store_id, String orderDate);
 
+    @Query("SELECT SUM(o.boxesAmount), SUM(o.totalPrice) FROM Order o WHERE o.store.id = ?1 AND o.orderDate = ?2")
+    String getDayHistoryDetails(Long store_id, String orderDate);
+
 }
