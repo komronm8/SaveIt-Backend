02cec3b6386823b5806f1e867da545587d312ff6
Komron <komron.ba@gmail.com>
2023-03-05 16:27:08 +0100

    Created a class for updating the current Box amounts of every Store

---
+++ src/main/java/com/example/SaveItBackend/Store/StoreBoxScheduler.java
@@ -0,0 +1,30 @@
+package com.example.SaveItBackend.Store;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Component
+@EnableScheduling
+public class StoreBoxScheduler {
+
+    private final StoreRepository storeRepository;
+
+    public StoreBoxScheduler(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 * * * *" )
+    public void updateBoxAmounts(){
+        Integer numberOfStores = storeRepository.getStoreById();
+        for(int i = 1; i <= numberOfStores; i++){
+            Store store = storeRepository.getReferenceById((long) i);
+            store.setCurrentBoxesAmount(store.getDefaultBoxesAmount());
+        }
+    }
+
+}
