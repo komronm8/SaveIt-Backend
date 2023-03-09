eea4bdb1a02677e864fe1671119b8476f108e425
Komron <komron.ba@gmail.com>
2023-03-10 00:57:14 +0100

    Added method getDataImageWithPath

--- src/main/java/com/example/SaveItBackend/Store/StoreConfig.java
+++ src/main/java/com/example/SaveItBackend/Store/StoreConfig.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -17,13 +18,21 @@ import java.util.List;
 @Configuration
 public class StoreConfig {
 
-    public byte[] imageData(String imageURL) throws IOException {
+    public byte[] getDataWithURL(String imageURL) throws IOException {
         BufferedImage bImage = ImageIO.read(new URL(imageURL));
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ImageIO.write( bImage, "png", bos);
         return bos.toByteArray();
     }
 
+    public byte[] getDataWithPath(String path) throws IOException{
+        File file = new File(path);
+        byte[] picInBytes = new byte[(int) file.length()];
+        FileInputStream fileInputStream = new FileInputStream(file);
+        fileInputStream.read(picInBytes);
+        fileInputStream.close();
+        return picInBytes;
+    }
 
     @Bean
     CommandLineRunner storeCommandLineRunner(StoreRepository repository){
@@ -44,27 +53,27 @@ public class StoreConfig {
                     "bakery,grocery,meal",
                     new BCryptPasswordEncoder().encode("klia0494"),
                     "Seattle-based coffeehouse chain known for its signature roasts, light bites and WiFi availability.",
-                    imageData("https://www.bundesverband-systemgastronomie.de/files/public/start/Logo_Banner/Starbucks.jpg"),
-                    imageData("https://images.unsplash.com/photo-1503481766315-7a586b20f66d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80")
+                    getDataWithURL("https://www.bundesverband-systemgastronomie.de/files/public/start/Logo_Banner/Starbucks.jpg"),
+                    getDataWithURL("https://images.unsplash.com/photo-1503481766315-7a586b20f66d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80")
             );
-            Store Coffeeboom = new Store(
-                    "Coffeeboom",
-                    "info@coffeeboom.com",
-                    "проспект Aль-Фараби 140а/3",
-                    "https://goo.gl/maps/BEa4kQZNSMuAshWB8",
-                    LocalDate.now(),
-                    1500.0,
-                    2000.0,
-                    LocalTime.of(21, 30),
-                    LocalTime.of(22, 00),
-                    5,
-                    5,
-                    "bakery,meal",
-                    new BCryptPasswordEncoder().encode("klia0494"),
-                    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit eum minus amet molestiae animi aliquid quisquam iusto harum dolorum qui repellendus laborum cupiditate, voluptatem magni modi perspiciatis a libero doloremque quam ab! Asperiores voluptatum, quaerat cumque nulla quod accusantium.",
-                    imageData("https://coffeeboom.kz/storage/app/uploads/public/5f7/e76/7e1/5f7e767e17f3d169479667.png"),
-                    imageData("https://images.unsplash.com/photo-1623334044303-241021148842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")
-            );
+//            Store Coffeeboom = new Store(
+//                    "Coffeeboom",
+//                    "info@coffeeboom.com",
+//                    "проспект Aль-Фараби 140а/3",
+//                    "https://goo.gl/maps/BEa4kQZNSMuAshWB8",
+//                    LocalDate.now(),
+//                    1500.0,
+//                    2000.0,
+//                    LocalTime.of(21, 30),
+//                    LocalTime.of(22, 00),
+//                    5,
+//                    5,
+//                    "bakery,meal",
+//                    new BCryptPasswordEncoder().encode("klia0494"),
+//                    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione odit eum minus amet molestiae animi aliquid quisquam iusto harum dolorum qui repellendus laborum cupiditate, voluptatem magni modi perspiciatis a libero doloremque quam ab! Asperiores voluptatum, quaerat cumque nulla quod accusantium.",
+//                    imageData("https://coffeeboom.kz/storage/app/uploads/public/5f7/e76/7e1/5f7e767e17f3d169479667.png"),
+//                    imageData("https://images.unsplash.com/photo-1623334044303-241021148842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")
+//            );
 //            Store LaTartine = new Store(
 //                    "La Tartine",
 //                    "info@la_tartine.com",
@@ -116,7 +125,7 @@ public class StoreConfig {
 //
 //
             repository.saveAll(
-                    List.of(Starbucks, Coffeeboom)
+                    List.of(Starbucks)
             );
 
         };
