d4c90ecf12fd87d4075dcf6b97d1270a554f0be7
Komron <komron.ba@gmail.com>
2023-06-03 23:32:11 +0200

    Minor changes made

--- src/main/java/com/example/SaveItBackend/SaveItBackendApplication.java
+++ src/main/java/com/example/SaveItBackend/SaveItBackendApplication.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.Duration;
-
 @SpringBootApplication
 public class SaveItBackendApplication {
 
